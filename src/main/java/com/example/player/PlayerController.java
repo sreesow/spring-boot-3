291186b8c2ebf10285fa9630c6ad9842120fdcb9
sree <sreesowmya2000@gmail.com>
2023-03-08 08:29:16 +0000

    solution

---
+++ src/main/java/com/example/player/PlayerController.java
@@ -0,0 +1,40 @@
+package com.example.player;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+import com.example.player.Player;
+import com.example.player.PlayerService;
+
+
+@RestController
+public class PlayerController {
+    PlayerService playerService = new PlayerService();
+
+    @GetMapping("/players")
+    public ArrayList<Player> getPlayers() {
+        return playerService.getPlayers();
+    }
+
+    @GetMapping("/players/{playerId}")
+    public Player getPlayerById(@PathVariable("playerId") int playerId) {
+        return playerService.getPlayerById(playerId);
+    }
+
+    @PostMapping("/players")
+    public Player addPlayer(@RequestBody Player player) {
+        return playerService.addPlayer(player);
+    }
+
+    @PutMapping("/players/{playerId}")
+    public Player updatePlayer(@PathVariable("playerId") int playerId, @RequestBody Player player) {
+        return playerService.updatePlayer(playerId,player);
+    }
+
+    
+    @DeleteMapping("/players/{playerId}")
+    public void deletePlayer(@PathVariable("playerId") int playerId){
+        playerService.deletePlayer(playerId);
+    }
+    
+}
